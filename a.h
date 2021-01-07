272b3ef3413c1e40534490f4c80aac9439bc9503
Archil Bolkvadze <a.bolkvadze@gmail.com>
2021-01-07 12:52:07 +0400

    sync

--- a.h
+++ a.h
@@ -7,21 +7,31 @@
 #define MO(ob) (((pith_t *)(ob))[0])
 #define L(n) NN(n, __LINE__)
 #define N(n) void n(pith_t o, void *s, void *e, void *b)
+#define NPS(g, f, body)                                                        \
+  static void NNNN(g, o, f, _pith)(int m, void *s, void *e, void *ob) {        \
+    pith_t o = MO(ob);                                                         \
+    void *b = MB(ob);                                                          \
+    if (m != 0)                                                                \
+      return o(m, s, e, b);                                                    \
+    body                                                                       \
+  }                                                                            \
+  static void NNN(g, o, f)(pith_t o, void *s, void *e, void *b) {              \
+    f(NNNN(g, o, f, _pith), s, e, (void *[]){o, b});                           \
+  }
 #define PS(g, f, ...)                                                          \
-  void NNNN(g, o, f, _pith)(int m, void *s, void *e, void *ob) {               \
+  static void NNNN(g, o, f, _pith)(int m, void *s, void *e, void *ob) {        \
     pith_t o = MO(ob);                                                         \
     void *b = MB(ob);                                                          \
     if (m != 0)                                                                \
       return o(m, s, e, b);
 #define PE(g, f)                                                               \
   }                                                                            \
-  void NNN(g, o, f)(pith_t o, void *s, void *e, void *b) {                     \
+  static void NNN(g, o, f)(pith_t o, void *s, void *e, void *b) {              \
     f(NNNN(g, o, f, _pith), s, e, (void *[]){o, b});                           \
   }
 #define P(T, v)                                                                \
   *(T *)s = v;                                                                 \
   s += sizeof(T);
-
 #define A(T, n)                                                                \
   T n = *(T *)s;                                                               \
   s += sizeof(T);
@@ -40,26 +50,26 @@
   }
 typedef void (*pith_t)(int, void *, void *, void *);
 #define A2(t, a, b)                                                            \
-  A(t, a)                                                                     \
+  A(t, a)                                                                      \
   A(t, b)
 #define A4(t, a, b, c, d)                                                      \
-  A2(t, a, b)                                                                 \
+  A2(t, a, b)                                                                  \
   A2(t, c, d)
 #define A8(t, a, b, c, d, e, f, g, h)                                          \
-  A4(t, a, b, c, d)                                                           \
+  A4(t, a, b, c, d)                                                            \
   A4(t, e, f, g, h)
 #define A16(t, a1, b1, c1, d1, e1, f1, g1, h1, a2, b2, c2, d2, e2, f2, g2, h2) \
-  A8(t, a1, b1, c1, d1, e1, f1, g1, h1)                                       \
+  A8(t, a1, b1, c1, d1, e1, f1, g1, h1)                                        \
   A8(t, a2, b2, c2, d2, e2, f2, g2, h2)
 #define P2(t, a, b)                                                            \
-  P(t, a)                                                                     \
+  P(t, a)                                                                      \
   P(t, b)
 #define P4(t, a, b, c, d)                                                      \
-  P2(t, a, b)                                                                 \
+  P2(t, a, b)                                                                  \
   P2(t, c, d)
 #define P8(t, a, b, c, d, e, f, g, h)                                          \
-  P4(t, a, b, c, d)                                                           \
+  P4(t, a, b, c, d)                                                            \
   P4(t, e, f, g, h)
 #define P16(t, a1, b1, c1, d1, e1, f1, g1, h1, a2, b2, c2, d2, e2, f2, g2, h2) \
-  P8(t, a1, b1, c1, d1, e1, f1, g1, h1)                                       \
+  P8(t, a1, b1, c1, d1, e1, f1, g1, h1)                                        \
   P8(t, a2, b2, c2, d2, e2, f2, g2, h2)
--- met.c
+++ met.c
@@ -1,11 +1,9 @@
-#include "SDL_error.h"
-#include "SDL_video.h"
 #include "a.h"
-#include <GL/gl.h>
-#include <SDL2/SDL.h>
-#include <stdio.h>
+//#include <GL/gl.h>
+//#include <SDL2/SDL.h>
+//#include <stdio.h>
 
-//N(window) {
+// N(window) {
 //  if (SDL_Init(SDL_INIT_EVERYTHING) != 0)
 //    return o(-1, s, e, b);
 //  SDL_Window *window =
@@ -17,9 +15,9 @@
 //  }
 //  SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
 //  SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3);
-//  SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
-//  SDL_GLContext *context = SDL_GL_CreateContext(window);
-//  if (context == 0) {
+//  SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK,
+//  SDL_GL_CONTEXT_PROFILE_CORE); SDL_GLContext *context =
+//  SDL_GL_CreateContext(window); if (context == 0) {
 //    SDL_DestroyWindow(window);
 //    SDL_Quit();
 //    return o(-1, s, e, b);
@@ -31,19 +29,61 @@
 //  SDL_DestroyWindow(window);
 //  SDL_Quit();
 //}
-void* cf();
-void* cg();
-void* ch();
-void df(void*);
-void dg(void*);
-void dh(void*);
-N(f){
-  void* p = cf();
-  if(p==0) o(-1,s,e,b); 
+#include <stdio.h>
+#include <stdlib.h>
+static void *cf() { return malloc(4); };
+static void *cg() { return malloc(4); };
+static void *ch() { return malloc(4); };
+static void df(void *p) { free(p); };
+static void dg(void *p) { free(p); };
+static void dh(void *p) { free(p); };
+static N(f) {
+  void *p = cf();
+  if (p == 0)
+    o(-1, s, e, b);
   void *start = s;
-  P(void*, p);
-  o(0,start,e,b);
+  P(void *, p);
+  o(0, start, e, b);
   df(p);
 }
+NPS(g, f, {
+  void *start = s;
+  A(void *, pp);
+  void *p = cg();
+  if (p == 0)
+    o(-1, s, e, b);
+  P(void *, p);
+  o(0, start, e, b);
+  dg(p);
+})
+PS(h, gof) {
+  void *start = s;
+  A(void *, p1);
+  A(void *, p2);
+  void *p = ch();
+  if (p == 0)
+    o(-1, s, e, b);
+  P(void *, p);
+  o(0, start, e, b);
+  dh(p);
+}
+PE(h, gof)
+PS(m, hogof) {
+  A(void *, p1);
+  A(void *, p2);
+  A(void *, p3);
 
-int main() { return 0; }
+  printf("%p %p %p\n", p1, p2, p3);
+  P4(void *, p1, p2, p3, 0);
+  o(4, s, e, b);
+}
+PE(m, hogof)
+static void pith(int m, void *s, void *e, void *b) { *((int *)b) += 1; }
+int main() {
+  void *m = malloc(1024);
+  void *s = m;
+  void *e = s + 1024;
+  int err = 0;
+  mohogof(pith, s, e, &err);
+  return err;
+}
